0102da7a4454c97c5b43fe8938a2bdf9ba8e4cbb
Michelle Wan <mwan12@my.bcit.ca>
2018-01-15 15:28:01 -0800

    Lab 1 submission.

--- Hello World/Hello World/lab1.cpp
+++ Hello World/Hello World/lab1.cpp
@@ -6,10 +6,36 @@
 // PRE: b is a positive integer
 // POST: a and b are unchanged
 // RETURN: the greatest common denominator of a and b.
-int gcd(const int a, const int b)
-{
+int gcd(const int a, const int b){
+
+    int greatest{1};
+	int a_factor;
+	int b_factor;
 	// Replace this line with your code (have fun!).
-	return 0;
+
+    for (int i = 1 ; i < a; i++) {
+		if (a % i == 0) {
+			a_factor = a / i;
+		}
+		else {
+			continue;
+		}
+        for (int j = 1; j < b; j++) {
+			if (b % j == 0) {
+				b_factor = a / j;
+			}
+			else {
+				continue;
+			}
+			if (a_factor == b_factor && a_factor > greatest) {
+				greatest = a_factor;
+			}
+        }
+    }
+    
+
+	return greatest;
+
 }
 
 // Fibonacci
@@ -19,5 +45,8 @@ int gcd(const int a, const int b)
 long long fibonacci(const int n)
 {
 	// Replace this line with your code (challenge: do this with recursion!).
-	return 0;
+	if (n == 2|| n == 1) {
+		return 1;
+	}
+	return fibonacci(n-1) + fibonacci(n-2);
 }
